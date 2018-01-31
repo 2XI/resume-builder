71fa705383c6714613487df7c24e242ea9b4982f
2XI <shengxi_hu@yeah.net>
2018-01-31 21:57:25 +0800

    配置仓库

---
+++ src/pages/Page1/Page1.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import style from './Page1.css'
+import image from './images/brickpsert.jpg'
+
+export default class Page1 extends Component {
+    render() {
+        return (
+            <div className={ style.box }>
+                this is Page1~
+                <img src={image}/>
+            </div>
+        )
+    }
+}
